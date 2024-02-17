50b1e0bb89319afd6cbbcad32c89ea2e85c139e5
Radium <2043869032@qq.com>
2024-02-17 21:33:05 +0800

    chore: commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-const App: React.FC = ()=>{
-  return <div></div>
-}
+import React from 'react';
 
+const App: React.FC = () => {
+  return <div></div>;
+};
 
-export default App
+export default App;
